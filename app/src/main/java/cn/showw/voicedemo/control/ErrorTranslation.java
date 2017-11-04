fa4a6882da575912dec995ca88ccd9dfd24e16f5
hejiayi <janreyho@gmail.com>
2017-11-04 15:50:03 +0800

    1  结合百度官方demo  最小集     deleted:    control/MyRecognizer.java     deleted:    recognization/ChainRecogListener.java     deleted:    recognization/PidBuilder.java 2  修改测试UI     在layout.activity_main.xml中，用ScrollView包装TextView

--- app/src/main/java/cn/showw/voicedemo/control/ErrorTranslation.java
+++ app/src/main/java/cn/showw/voicedemo/control/ErrorTranslation.java
@@ -11,31 +11,31 @@ public class ErrorTranslation {
     public static String recogError(int errorCode){
         String message;
         switch (errorCode) {
-            case SpeechRecognizer.ERROR_AUDIO:
+            case SpeechRecognizer.ERROR_AUDIO:              //3
                 message = "音频问题";
                 break;
-            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:     //6
                 message = "没有语音输入";
                 break;
-            case SpeechRecognizer.ERROR_CLIENT:
+            case SpeechRecognizer.ERROR_CLIENT:             //5
                 message = "其它客户端错误";
                 break;
-            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS://9
                 message = "权限不足";
                 break;
-            case SpeechRecognizer.ERROR_NETWORK:
+            case SpeechRecognizer.ERROR_NETWORK:            //2
                 message = "网络问题";
                 break;
-            case SpeechRecognizer.ERROR_NO_MATCH:
+            case SpeechRecognizer.ERROR_NO_MATCH:           //7
                 message = "没有匹配的识别结果";
                 break;
-            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:    //8
                 message =  "引擎忙";
                 break;
-            case SpeechRecognizer.ERROR_SERVER:
+            case SpeechRecognizer.ERROR_SERVER:             //4
                 message = "服务端错误";
                 break;
-            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:    //1
                 message = "连接超时";
                 break;
             default:
